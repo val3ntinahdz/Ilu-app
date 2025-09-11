1f3bf54679f018f2841b9ac6789e779397f5fc2c
SaulAgr <saulabigaelguerrarivas@gmail.com>
2025-09-11 14:04:10 -0600

    Create homepage: pt 1

---
+++ frontend/src/homePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div style={{ padding: '2rem', color: 'var(--ilu-light)' }}>
+      <p>Hola Mundo</p>
+    </div>
+  )
+}
+
+export default HomePage
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './App.jsx'
+import HomePage from './homePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+   {/* <Login />*/}
+   {/*<HomePage />*/}
+   <Login />
+    
+    
   </StrictMode>,
 )
