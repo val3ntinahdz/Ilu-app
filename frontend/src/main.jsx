d7b6d5a5e0fae92f90d94a86d0bb79bcb399db0a
SaulAgr <saulabigaelguerrarivas@gmail.com>
2025-09-11 16:51:15 -0600

    Version 3 de HomePage estilos actualizados

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import Login from './App.jsx'
 import HomePage from './homePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    {/* <Login />*/}
-   {/*<HomePage />*/}
-   <Login />
+   <HomePage />
     
     
   </StrictMode>,
